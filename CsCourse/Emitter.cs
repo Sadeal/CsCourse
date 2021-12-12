e30d2f08f7c1a17350f6d1dc850237340a38c333
Alexander <91420542+Sadeal@users.noreply.github.com>
2021-12-12 13:36:53 +0800

    Вроде что-то

--- CsCourse/Emitter.cs
+++ CsCourse/Emitter.cs
@@ -13,9 +13,43 @@ namespace CsCourse
 
         public int MousePositionX = 0;
         public int MousePositionY = 0;
+        public float GravitationX = 0;
+        public float GravitationY = 1;
+        public int _x; // координата X центра эмиттера, будем ее использовать вместо MousePositionX
+        public int _y; // соответствующая координата Y 
+        public int _direction = 0; // вектор направления в градусах куда сыпет эмиттер
+        public int _spreading = 360; // разброс частиц относительно Direction
+        public int _speedMin = 1; // начальная минимальная скорость движения частицы
+        public int _speedMax = 10; // начальная максимальная скорость движения частицы
+        public int _radiusMin = 2; // минимальный радиус частицы
+        public int _radiusMax = 10; // максимальный радиус частицы
+        public int _lifeMin = 20; // минимальное время жизни частицы
+        public int _lifeMax = 100; // максимальное время жизни частицы
+        public int _particlesPerTick = 1;
+
+        public virtual void ResetParticle(Particle particle)
+        {
+            particle._life = Particle.rand.Next(_lifeMin, _lifeMax);
+
+            particle._x = _x;
+            particle._y = _y;
+
+            var direction = _direction
+                + (double)Particle.rand.Next(_spreading)
+                - _spreading / 2;
+
+            var speed = Particle.rand.Next(_speedMin, _speedMax);
+
+            particle._speedX = (float)(Math.Cos(direction / 180 * Math.PI) * speed);
+            particle._speedY = -(float)(Math.Sin(direction / 180 * Math.PI) * speed);
+
+            particle._radius = Particle.rand.Next(_radiusMin, _radiusMax);
+        }
 
         public void UpdateState()
         {
+            int particlesToCreate = _particlesPerTick;
+
             foreach (var particle in particles)
             {
                 particle._life -= 1;
@@ -29,9 +63,16 @@ namespace CsCourse
                     particle._speedX = (float)(Math.Cos(_direction / 180 * Math.PI) * _speed);
                     particle._speedY = -(float)(Math.Sin(_direction / 180 * Math.PI) * _speed);
                     particle._radius = 2 + Particle.rand.Next(10);
+                    if(particlesToCreate > 0)
+                    {
+                        particlesToCreate -= 1;
+                        ResetParticle(particle);
+                    }
                 }
                 else
                 {
+                    particle._speedX += GravitationX;
+                    particle._speedY += GravitationY;
                     particle._x += particle._speedX;
                     particle._y += particle._speedY;
                 }
@@ -41,7 +82,10 @@ namespace CsCourse
             {
                 if (particles.Count < 500)
                 {
-                    var particle = new Particle();
+                    var particle = new ParticleColorful();
+                    particles.Add(particle);
+                    particle.FromColor = Color.Yellow;
+                    particle.ToColor = Color.FromArgb(0, Color.Magenta);
                     particle._x = MousePositionX;
                     particle._y = MousePositionY;
                     particles.Add(particle);
@@ -51,8 +95,14 @@ namespace CsCourse
                     break;
                 }
             }
+            /*while(particlesToCreate >= 1)
+            {
+                particlesToCreate -= 1;
+                var particle = CreateParticle();
+                ResetParticle(particle);
+                particles.Add(particle);
+            }*/
         }
-
         public void Render(Graphics g)
         {
             foreach (var particle in particles)
--- CsCourse/Particle.cs
+++ CsCourse/Particle.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Drawing.Drawing2D;
 
 namespace CsCourse
 {
@@ -17,8 +18,28 @@ namespace CsCourse
         public float _speedY;
         public float _amount; // добавить
         public float _life;
+        public Color _color = Color.Red;
 
         public static Random rand = new Random();
+
+        public virtual GraphicsPath GetGraphicsPath()
+        {
+            // пока возвращаем пустую форму
+            return new GraphicsPath();
+        }
+
+        public virtual bool Overlaps(Particle obj, Graphics g)
+        {
+            // берем информацию о форме
+            var path1 = this.GetGraphicsPath();
+            var path2 = obj.GetGraphicsPath();
+
+            // используем класс Region, который позволяет определить 
+            // пересечение объектов в данном графическом контексте
+            var region = new Region(path1);
+            region.Intersect(path2); // пересекаем формы
+            return !region.IsEmpty(g); // если полученная форма не пуста то значит было пересечение
+        }
 
         public Particle()
         {
@@ -30,7 +51,7 @@ namespace CsCourse
             _radius = 2 + rand.Next(10);
             _life = 20 + rand.Next(100);
         }
-        public void Draw(Graphics g)
+        public virtual void Draw(Graphics g)
         {
             // рассчитываем коэффициент прозрачности по шкале от 0 до 1.0
             float k = Math.Min(1f, _life / 100);
@@ -43,6 +64,38 @@ namespace CsCourse
             g.FillEllipse(SB, _x - _radius, _y - _radius, _radius * 2, _radius * 2); // залитый круг с фикс. радиусом
 
             SB.Dispose(); // удаление кисти (free spacing)
+        }
+        public void SetColor(Color color)
+        {
+            _color = color;
+        }
+    }
+    public class ParticleColorful : Particle
+    {
+        // два новых поля под цвет начальный и конечный
+        public Color FromColor;
+        public Color ToColor;
+
+        // для смеси цветов
+        public static Color MixColor(Color color1, Color color2, float k)
+        {
+            return Color.FromArgb(
+                (int)(color2.A * k + color1.A * (1 - k)),
+                (int)(color2.R * k + color1.R * (1 - k)),
+                (int)(color2.G * k + color1.G * (1 - k)),
+                (int)(color2.B * k + color1.B * (1 - k))
+            );
+        }
+
+        public override void Draw(Graphics g)
+        {
+            float k = Math.Min(1f, _life / 100);
+
+            // так как k уменьшается от 1 до 0, то порядок цветов обратный
+            var color = Color.Blue;
+            var SB = new SolidBrush(color);
+            g.FillEllipse(SB, _x - _radius, _y - _radius, _radius * 2, _radius * 2);
+            SB.Dispose();
         }
     }
 }
